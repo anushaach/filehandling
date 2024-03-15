6b6f4d678460d7a722f87b787c2eda3a6e312826
Anusha Acharya <104045774+anushaach@users.noreply.github.com>
2024-03-15 13:37:51 +0545

    delete and written file

--- demo.dart
+++ demo.dart
@@ -1,11 +1,26 @@
-import "dart:io";
+// import "dart:io";
+
+// void main() {
+//   File file = File("detial.csv");
+//   String contents = file.readAsStringSync();
+//   print(contents);
+//   print('File path:${file.path}');
+//   print('File absolute path:${file.absolute.path}');
+//   print('File size:${file.lengthSync()}kb');
+//   print('Last modified:${file.lastModifiedSync()}');
+// }
+import 'dart:io';
 
 void main() {
-  File file = File("detial.csv");
-  String contents = file.readAsStringSync();
-  print(contents);
-  print('File path:${file.path}');
-  print('File absolute path:${file.absolute.path}');
-  print('File size:${file.lengthSync()}kb');
-  print('Last modified:${file.lastModifiedSync()}');
+  File file = File('test.txt');
+  file.writeAsStringSync("Welcome to our class");
+  print('File written');
 }
+
+// import 'dart:io';
+
+// void main() {
+//   File file = File('test.txt');
+//   file.deleteSync();
+//   print('File delete');
+// }
